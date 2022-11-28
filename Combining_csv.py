ef65b6c26abc7e86eee790309485a17bd019bff2
Mvellimana <90660051+Mvellimana@users.noreply.github.com>
2022-11-28 14:22:59 -0600

    Update Combining_csv.py

--- Combining_csv.py
+++ Combining_csv.py
@@ -7,6 +7,6 @@ extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
 #combine all files in the list
-yelp_all_reviews = pd.concat([pd.read_csv(f,encoding = 'latin1') for f in all_filenames],axis = 0)
+yelp_all_reviews = pd.concat([pd.read_csv(f,encoding = 'ISO-8859-1') for f in all_filenames],axis = 0)
 #export to csv
 yelp_all_reviews.to_csv( "yelp_all_reviews.csv", index=False, encoding='utf-8-sig')
