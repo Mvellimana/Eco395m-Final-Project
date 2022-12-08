b80480c40021a00cdd3406c2b9c5f29f598a5fdc
Ahmed Almezail <98285671+Mezalay@users.noreply.github.com>
2022-12-08 15:43:11 -0600

    Add files via upload

--- code_streamlit/streamlit_app.py
+++ code_streamlit/streamlit_app.py
@@ -10,22 +10,13 @@ import os
 
 
 
-PATH5 = os.path.join("artifacts", "yelp_map_data.csv")
-
-df5 = (
-	pd.read_csv(PATH5)
-	)
-
-
-
-
 
 with st.sidebar:
 	st.image("https://s3-media2.fl.yelpcdn.com/bphoto/HHyTr44O1_Xp8sYuTpKU8g/o.jpg")
 	choice=option_menu(
 		menu_title="Final Project",
-		options=["Overview","Word Clouds","Sentiment Analysis","Choose Your Restaurants","Food Map"],
-		icons=["house","book","list","list-task","map"],
+		options=["Overview","Restaurants Map", "Top Mentioned Words","Choose Your Restaurants", "Sentiment Analysis"],
+		icons=["house", "map", "book", "list-task", "list"],
 		menu_icon="cast",
 		default_index=0,
 		)
@@ -37,6 +28,8 @@ def load_lottiefile(filepath: str):
 		return json.load(f)
 
 if choice=="Overview":
+
+	st.title("Opinions Mining of Yelp Reviews")
 	
 	lottie_coding=load_lottiefile("animation.json")
 	st_lottie(
@@ -50,15 +43,79 @@ if choice=="Overview":
 		key=None,
 	)
 
+	st.header("Introduction")
+	st.markdown("...")
+
+
+	st.header("Data Source")
+	st.subheader("API Scrapping")
+	st.markdown("To start this project, we began with using Yelp’s Fusion API system to gather business ID’s as well as a JSON file associated with those IDs.\
+	 This JSON file contained information about the restaurant such as the name of the restaurant, number of reviews, ratings, location, etc. \
+	 We followed up this by then using the ID to also gather up to three reviews per restaurant (Yelp’s API limit). \
+	 Sadly Yelp also limited the maximum number of characters in a review. Overall, this limited the possibilities of what we could do with sentiment analysis. \
+	 That’s when a beacon was lit and Super Scraper Jordan came to our rescue. \
+	 Using web scraping, we were able to come up with essentially all the reviews for each Mexican restaurant in Austin.")
+
+	st.subheader("GCP Database")
+	st.markdown("For our database we used a PostgreSQL 13 server on the Google Cloud Platform.\
+		Which in hindsight might have been a little more powerful than we needed but the server was still cheaper than expected.")
+
+	st.header(" Data Exploration")
+	st.markdown("Word clouds (food/reviews) In our daily life, it can be easily understood that one user who is considering what to eat for a meal may come up with ideas referring to other users frequent comments.\
+ Words Cloud presenting most frequent search keywords by other users provide an intuitive way. Aiming at giving users more references, we offer 2 choices: \
+ one by food and one by reviews.")
+	st.markdown("Sometimes users tend to make their specific choice before they \
+		begin to find a particular interested restaurant, which makes a filtration function according to a user’s concern important. \
+		We offer  such a function with a dropoff menu in our streamlit dashboard.There are 4 attributes in total. \
+		Users could choose randomly 1-4 of them, then only  restaurants satisfy all attributes chosen by users appear.")
+
+	st.markdown("Sometimes users are price sensitive, also sometimes users show great interest in restaurants above a particular rating. \
+		Nobody wants to pick a low rating restaurant with a higher price. In a word, \
+		providing price choice as well as rating choice is meaningful to help a user to find a final destination to eat. In our dashboard, \
+		we provide this kind of choice combination. Users can freely either make their choices by price filtration or rating filteration, \
+		or even combining these two together to filter.")
+
+
+	st.header("Methdology")
+	st.markdown("Online reviews have the power to drive customers to or away from a business, \
+		and tell you what customers like and dislike about it. We performed sentiment and entity analysis on restaurant reviews to uncover emotions in online reviews, \
+		to detect trends and patterns that may not be evident at first glance. We also tested the accuracy of the sentiment score using \
+		TextBlob’s pre-built model and a Hand-Built Naïve Bayes TF IDF (Bag-of-Words) Model. Our approaches are elaborated in further detail in the sections below.")
+
+	st.subheader("Sentiment Analysis")
+	st.markdown("Sentiment Analysis provides an effective way to evaluate the sentiment behind text. \
+		It typically uses machine learning algorithms and natural language processing (NLP) to estimate whether the data is positive, \
+		neutral or negative")
+ 
+st.markdown("Overall Review Sentiment Analysis")
+
+
+
+
+PATH5 = os.path.join("artifacts", "yelp_map_data.csv")
+
+df5 = (
+	pd.read_csv(PATH5)
+	)
+
+if choice=="Restaurants Map":
+	st.header("Locate the Highest Ratings Mexican Restaurants in Austin")
+	st.markdown('Choose the minimum rating.')
+
+	rating = st.slider("Rating", 1.0, float(df5["rating"].max()), 4.5, step = 0.5)
+	st.map(df5.query("rating >= @rating")[["latitude", "longitude"]])
+	
 
 
 PATH3 = os.path.join("artifacts", "wordcloud_food.png")
 PATH2 = os.path.join("artifacts", "wordcloud_reviews.png")
 
 
-if choice=="Word Clouds":
 
-	st.title("this is title")
+
+if choice=="Top Mentioned Words":
+
+	st.title("Top Words/Dishes")
 	lottie_coding=load_lottiefile("star.json")
 	st_lottie(
 		lottie_coding,
@@ -71,20 +128,20 @@ if choice=="Word Clouds":
 		key=None,
 	)
 
+	st.markdown("This analysis is conducted based on scrapped yelp reviews on Mexican restaurants in Austin. Click on the buttons below to explore them. :smile:")
+	st.subheader("Top Mentioned Dishes")
 	
-	st.subheader("this is subheader")
-	st.markdown("this is text to explain this part shortly")
 
 	if st.button("Top Mentioned Dishes"):
 		st.image(PATH3)
 
-	st.subheader("this is subheader")
-	st.markdown("this is text to explain this part shortly")
+	st.subheader("Top Iterated Words Reviews")
+	
 
 	if st.button("Top Iterated Words Reviews"):
 		st.image(PATH2)
 
-	st.markdown("this is text to explain this part shortly")
+	
 
 
 PATH4 = os.path.join("artifacts", "sentiment_score_by_rest.csv")
@@ -94,44 +151,20 @@ df4 = (
 	)
 
 
-if choice=="Sentiment Analysis":
-
-	st.title("this is title")
-
-	lottie_coding=load_lottiefile("avocado.json")
-	st_lottie(
-		lottie_coding,
-		speed=1,
-		reverse=False,
-		loop=True,
-		quality="low",
-		height=250,
-		width=350,
-		key=None,
-	)
-
-	st.header("this is header")
-	st.markdown("this is text to explain this part shortly")
-	col1, col2 = st.columns(2)
-	col1.metric("Pre-Built Algorthim", "89.67% Accuracy", "TextBlob")
-	col2.metric("Hand-Built Algorthim", "83.8% Accuracy", "Naive Bayes- TF IDF")
-	
-
-	st.header("this is header here")
-	st.subheader("Slide to your Favorite")
-	score=st.slider("All the restaurants above the sentiment score you select",min_value=-1.0,max_value=1.0,value=0.00,step=0.001)
-	df4_made=df4.loc[df4["Polarity"]>=score]
-	st.dataframe(df4_made)
-	st.image("https://s3-media1.fl.yelpcdn.com/bphoto/Fg2LTmPtlDLo3eCFw_V6Cw/o.jpg",width=520)
 
 
 PATH6 = os.path.join("artifacts", "mexican_restaurant_attributes.csv")
 PATH7 = os.path.join("artifacts", "mexican_restaurant_info.csv")
-	
-if choice=="Choose Your Restaurants":	
-	st.image("https://s3-media4.fl.yelpcdn.com/bphoto/XkJSex0R3IgwnO8i4UG2AQ/o.jpg",width=200)
 
-	attribute_choice=st.multiselect("Tell us your choice",["Takes Reservations", "Offers Delivery","Offers Takeout", "Masks required"])
+
+
+if choice=="Choose Your Restaurants":
+	st.title("Choose Your Restaurants")
+
+	st.image("https://s3-media4.fl.yelpcdn.com/bphoto/XkJSex0R3IgwnO8i4UG2AQ/o.jpg",width=200)
+	st.subheader("Select your Favorite Restaurants Based on Attributes")
+
+	attribute_choice=st.multiselect("Filter by attributes",["Takes Reservations", "Offers Delivery","Offers Takeout", "Masks required"])
     
 	
 	df6 = pd.read_csv(PATH6)
@@ -155,10 +188,12 @@ if choice=="Choose Your Restaurants":
 	
 	a,b=st.columns([3,1])
 	money_choice=a.selectbox("Price Range",[" ","$$$","$$","$"]) 
-	rating_choice = b.number_input("Rating Above", step=0.5)
+	rating_choice = b.number_input("Minimum Rating", step=0.5)
 
 
 	df_info = pd.read_csv(PATH7)
+
+	st.subheader("Select your Favorite Restaurants Based on Price and Rating")
 
 	def price_check(price, price_choice):
 		'''get the boolean values for price.'''
@@ -183,14 +218,73 @@ if choice=="Choose Your Restaurants":
 	st.dataframe(df_limit_cols[bool_select])
 
 
+PATH8 = os.path.join("artifacts", "best_rest.csv")
+PATH9 = os.path.join("artifacts", "worst_rest.csv")	
+
+
+if choice=="Sentiment Analysis":
+
+	st.title("Sentiment Analysis")
+
+	lottie_coding=load_lottiefile("avocado.json")
+	st_lottie(
+		lottie_coding,
+		speed=1,
+		reverse=False,
+		loop=True,
+		quality="low",
+		height=250,
+		width=350,
+		key=None,
+	)
+
+	st.header("Pre-Built Algorthim vs Hand-Built Algorthim")
+	st.markdown("We have conducted 2 different models to assess the sentiment analysis. The first model is a **TextBlob** model which is a *Pre-Built Algorthim* that uses\
+		reviews text as input. The other model is a **Naive Bayes- TF IDF** which is a *Hand-Built Model* that uses rating as input which could be a proxy for reviewers sentiment.\
+		The objective is to compare the accuracy of both models.")
+	col1, col2 = st.columns(2)
+	col1.metric("Pre-Built Algorthim", "89.67% Accuracy", "TextBlob")
+	col2.metric("Hand-Built Algorthim", "83.8% Accuracy", "Naive Bayes- TF IDF")
+	st.markdown("So, we are using the **TextBlob** model results to show them below.")
+	
+
+	st.header("Find the Sentiment Score for Mexican Restaurants")
+	st.markdown("Slide right for restaurants with higher sentiment analysis, and left to include the lower.")
+	score=st.slider("Select the minimum sentiment score.",min_value=-1.0,max_value=1.0,value=0.00,step=0.001)
+	df4_made=df4.loc[df4["Polarity"]>=score]
+	st.dataframe(df4_made)
+	
+	
+
+	st.header("Find the Best and the worst restaurants from the selectbox")
+	st.markdown("Pick a specific dish to find the top 5 and worst 5 restaurants.")
+	df_best = pd.read_csv(PATH8)
+	df_worst = pd.read_csv(PATH9)
+
+	dish = st.selectbox(
+	    "Choose dishes you want, Find the Best and Leave the Worst!",
+	    [' ','tortilla', 'burrito', 'quesadilla', 'steak','fish', 'enchilada', 'fajita', 'taco', 'chicken', 'shrimp'])
+	
+	def food_check(food, food_choice):
+	    return str(food_choice) == str(food)
+
+	if dish == ' ':
+	    food_bool = [False] * len(df_best.index)
+	elif dish != ' ':
+	    food_bool_best = df_best['cleaan_entity_name'].apply(food_check, args=(dish, ))
+	    food_bool_worst = df_best['cleaan_entity_name'].apply(food_check, args=(dish, ))
+	    df_1 = df_best[food_bool_best]
+	    df_2 = df_worst[food_bool_worst]
+	    df_comb = df_1.append(df_2)
+	    st.dataframe(df_comb)
 
 
 
-if choice=="Food Map":
-	st.header("Choose the Minimum Rating for your Favorite Mexican Restaurants")
-	rating = st.slider("Rating", 1.0, float(df5["rating"].max()), 4.5, step = 0.5)
-	st.map(df5.query("rating >= @rating")[["latitude", "longitude"]])
-	st.markdown('jkdfkjf')
+
+
+
+
+
 
 
 
