57d445b3ee4184cdb793fc318e217f36a4e0a57b
Tyler Nicholson <tnicholson@ksdynamics.com>
2022-12-07 21:32:39 -0600

    changed name of the code (Yelp API) folder to code_Yelp_API to make relative paths easier.

--- code_Yelp_API/scrape_businesses.py
+++ code_Yelp_API/scrape_businesses.py
@@ -7,7 +7,7 @@ from yelp_requests import business_search
 if __name__ == "__main__":
 
     LOCATION = "Austin, TX"
-    CATEGORIES = "pizza"
+    CATEGORIES = "asianfusion"
 
     create_tables()
 
