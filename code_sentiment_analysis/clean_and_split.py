e9d3fb7f5c02c58ac41fa8b082bc82466ab8596b
sonali4794 <sonalimishra.nitt@gmail.com>
2022-12-09 02:10:08 +0530

    added comments to the code

--- code_sentiment_analysis/clean_and_split.py
+++ code_sentiment_analysis/clean_and_split.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-
+#read the csv and reduce the sample size to focus on 10 categories that we manually chose based on frequency
 review_data_original=pd.read_csv('artifacts/mexican_reviews_cleaned.csv')
 
 list1=['taco','tortilla','chicken','enchilada','fajita','burrito','fish','shrimp','quesadilla','steak',
@@ -13,6 +13,7 @@ list1=['taco','tortilla','chicken','enchilada','fajita','burrito','fish','shrimp
 review_data_original['check']=review_data_original.review.apply(lambda x: 1 if any(substring in x for substring in list1)==True else 0)
 review_data_big = review_data_original[review_data_original['check']==1]
 
+#we split the data into 4 parts in order to evenly distribute the credits usage on GCP NLP 
 review_data_big_split = np.array_split(review_data_big, 4)
 
 review_data_part1 = review_data_big_split[0].drop(['Unnamed: 0'], axis=1)
